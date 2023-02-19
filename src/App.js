b4e175281cfeb7e83e22dd7afaca7dc1e94af487
splean101 <101splean@gmail.com>
2023-02-19 13:34:51 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Article from "./components/Article";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <h1 className="title">NVIDIA news</h1>
+      <div className="article">
+      <Article />
+      </div>
+    </div> 
+    );
+  }
+export default App;
